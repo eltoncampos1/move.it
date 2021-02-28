bd8de1ae6688e6ea9229f8e5e14f1d1f0c199ef1
elton de campos <eltoncampos36@gmail.com>
2021-02-28 00:57:11 -0300

    chore: create complated challenges css module and import is respective file

--- src/components/CompletedChallenges.tsx
+++ src/components/CompletedChallenges.tsx
@@ -1,6 +1,8 @@
+import styles from "../styles/components/CompletedChallenges.module.css";
+
 export function CompletedChallenges() {
   return (
-    <div>
+    <div className={styles.completedChallengesContainer}>
       <span>Completed Challenges</span>
       <span>10</span>
     </div>
