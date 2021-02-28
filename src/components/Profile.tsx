fece79e8bf2022c441990993b7c24e099d3bad06
elton de campos <eltoncampos36@gmail.com>
2021-02-28 00:49:33 -0300

    chore: add level icon

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,10 +1,15 @@
+import styles from "../styles/components/Profile.module.css";
+
 export function Profile() {
   return (
-    <div>
-      <img src="https://github.com/eltoncampos1.png" alt="" />
+    <div className={styles.profileContainer}>
+      <img src="https://github.com/eltoncampos1.png" alt="Elton Campos" />
       <div>
         <strong>Elton Campos</strong>
-        <p>Level 1</p>
+        <p>
+          <img src="icons/level.svg" alt="level icon" />
+          Level 1
+        </p>
       </div>
     </div>
   );
