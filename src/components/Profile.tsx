7acd6631e999ac847bb16be9c99f40fabbfb101b
elton de campos <eltoncampos36@gmail.com>
2021-02-28 00:27:56 -0300

    feat: create a profile component

---
+++ src/components/Profile.tsx
@@ -0,0 +1,11 @@
+export function Profile() {
+  return (
+    <div>
+      <img src="https://github.com/eltoncampos1.png" alt="" />
+      <div>
+        <strong>Elton Campos</strong>
+        <p>Level 1</p>
+      </div>
+    </div>
+  );
+}
