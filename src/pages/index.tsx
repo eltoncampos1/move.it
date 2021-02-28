2d854bc9c4b72e1aa147279df1d23c90b04ee0f5
elton de campos <eltoncampos36@gmail.com>
2021-02-28 01:21:36 -0300

    chore: add a new Title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,10 +4,14 @@ import { CompletedChallenges } from "../components/CompletedChallenges";
 import { Countdown } from "../components/CountDown";
 
 import styles from "../styles/pages/Home.module.css";
+import Head from "next/head";
 
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Head>
+        <title>Início | move.it</title>
+      </Head>
       <ExperienceBar />
 
       <section>
