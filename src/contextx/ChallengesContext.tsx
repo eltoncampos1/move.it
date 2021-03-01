ed2eb683b9fb723eb963680b56c3f4d06249fb3c
elton de campos <eltoncampos36@gmail.com>
2021-03-01 00:39:22 -0300

    feat: create start new challenger function

--- src/contextx/ChallengesContext.tsx
+++ src/contextx/ChallengesContext.tsx
@@ -1,20 +1,54 @@
 import { createContext, ReactNode, useState } from "react";
+import challenges from "../../challenges.json";
 
-export const ChallengesContext = createContext({});
-
+interface Challenge {
+  type: "body" | "eye";
+  description: string;
+  amount: number;
+}
+interface ChallengesContextdata {
+  level: number;
+  currentExperience: number;
+  challengesCompleted: number;
+  activeChallenge: Challenge;
+  levelUp: () => void;
+  startNewChallenge: () => void;
+}
 interface ChallengesProviderProps {
   children: ReactNode;
 }
 
+export const ChallengesContext = createContext({} as ChallengesContextdata);
+
 export function ChallengesProvidder({ children }: ChallengesProviderProps) {
   const [level, setlevel] = useState(1);
+  const [currentExperience, setCurrentExperience] = useState(0);
+  const [challengesCompleted, setChallengesCompleted] = useState(0);
+
+  const [activeChallenge, setActiveChallenge] = useState(null);
 
   function levelUp() {
     setlevel(level + 1);
   }
 
+  function startNewChallenge() {
+    const randomChallengeIndex = Math.floor(Math.random() * challenges.length);
+    const challenge = challenges[randomChallengeIndex];
+
+    setActiveChallenge(challenge);
+  }
+
   return (
-    <ChallengesContext.Provider value={{ level, levelUp }}>
+    <ChallengesContext.Provider
+      value={{
+        level,
+        currentExperience,
+        challengesCompleted,
+        levelUp,
+        startNewChallenge,
+        activeChallenge,
+      }}
+    >
       {children}
     </ChallengesContext.Provider>
   );
