7470554c7991438aa80e11ce47afc987314e4af0
elton de campos <eltoncampos36@gmail.com>
2021-02-28 23:30:35 -0300

    chore: create challenge box active struvture

--- src/components/ChalengeBox/index.tsx
+++ src/components/ChalengeBox/index.tsx
@@ -1,15 +1,42 @@
-import { ChallengeBoxContainer, ChallengeNotActive } from "./styles";
+import {
+  ChallengeBoxContainer,
+  ChallengeNotActive,
+  ChallengeActive,
+  ChallengeBoxMain,
+  ChallengeBoxFooter,
+  FailedButton,
+  CompletedButton,
+} from "./styles";
 
 export function ChalengeBox() {
+  const hasActiveChallenge = true;
+
   return (
     <ChallengeBoxContainer>
-      <ChallengeNotActive>
-        <strong>Finish a cycle to receive a challenge</strong>
-        <p>
-          <img src="icons/level-up.svg" alt="Level Up" />
-          Level up by completing challenges.
-        </p>
-      </ChallengeNotActive>
+      {hasActiveChallenge ? (
+        <ChallengeActive>
+          <header>Earn 400 xp</header>
+
+          <ChallengeBoxMain>
+            <img src="icons/body.svg" />
+            <strong>New Challenge</strong>
+            <p>Lift and stretch for your back</p>
+          </ChallengeBoxMain>
+
+          <ChallengeBoxFooter>
+            <FailedButton type="button">Failed</FailedButton>
+            <CompletedButton type="button">Succeeded</CompletedButton>
+          </ChallengeBoxFooter>
+        </ChallengeActive>
+      ) : (
+        <ChallengeNotActive>
+          <strong>Finish a cycle to receive a challenge</strong>
+          <p>
+            <img src="icons/level-up.svg" alt="Level Up" />
+            Level up by completing challenges.
+          </p>
+        </ChallengeNotActive>
+      )}
     </ChallengeBoxContainer>
   );
 }
