d4300cd984a8544656a6d83fdd3048004145415c
elton de campos <eltoncampos36@gmail.com>
2021-02-28 23:00:38 -0300

    chore: create a finished cycle button style

--- src/components/Countdown/styles.ts
+++ src/components/Countdown/styles.ts
@@ -90,3 +90,26 @@ export const CountdownButtonActive = styled.button`
         color: var(--white);
     }
 `;
+
+export const CountdownButtonFinished = styled.button`
+   width: 100%;
+    height: 5rem;
+
+    margin-top: 2rem;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    border: 0;
+    border-radius: 5px;
+
+    background: var(--white);
+    color: var(--text);
+    cursor: not-allowed;
+
+    font-size: 1.25rem;
+    font-weight: 600;
+    transition: background-color 0.2s;
+
+`;
