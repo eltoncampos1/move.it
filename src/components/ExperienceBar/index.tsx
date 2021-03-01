5f38a5551d46a5eedf2b0d023b21fc51fc1a601c
elton de campos <eltoncampos36@gmail.com>
2021-03-01 00:48:13 -0300

    chore: add current experience state

--- src/components/ExperienceBar/index.tsx
+++ src/components/ExperienceBar/index.tsx
@@ -1,14 +1,19 @@
 // import styles from "../styles/components/ExperienceBar.module.css";
+import { useContext } from "react";
+import { ChallengesContext } from "../../contextx/ChallengesContext";
 import { ExpHeader, CurrentExperience } from "./styles";
 
 export function ExperienceBar() {
+  const { currentExperience } = useContext(ChallengesContext);
   return (
     <ExpHeader>
       <span>0 xp</span>
       <div>
         <div style={{ width: "50%" }} />
 
-        <CurrentExperience style={{ left: "50%" }}>300xp</CurrentExperience>
+        <CurrentExperience style={{ left: "50%" }}>
+          {currentExperience}
+        </CurrentExperience>
       </div>
       <span>600 xp</span>
     </ExpHeader>
