199f1c33fef2b8709665bd4fba2291433f3ef488
elton de campos <eltoncampos36@gmail.com>
2021-03-01 02:01:35 -0300

    reafctor: move some functions to countdown context

--- src/components/Countdown/index.tsx
+++ src/components/Countdown/index.tsx
@@ -1,5 +1,3 @@
-import { useContext, useEffect, useState } from "react";
-import { ChallengesContext } from "../../contextx/ChallengesContext";
 import {
   Container,
   CountdownButton,
@@ -7,41 +5,9 @@ import {
   CountdownButtonFinished,
 } from "./styles";
 
-let countDownTimeout: NodeJS.Timeout;
-
 export function Countdown() {
-  const { startNewChallenge } = useContext(ChallengesContext);
-  const [time, setTime] = useState(0.1 * 60);
-  const [isActive, setisActive] = useState(false);
-  const [hasFinished, sethasFinished] = useState(false);
-
-  const minutes = Math.floor(time / 60);
-  const seconds = time % 60;
-
   const [minuteLeft, minuteRight] = String(minutes).padStart(2, "0").split("");
   const [secondLeft, secondRight] = String(seconds).padStart(2, "0").split("");
-
-  function startCountdown() {
-    setisActive(true);
-  }
-
-  function resetCountdown() {
-    clearTimeout(countDownTimeout);
-    setisActive(false);
-    setTime(0.1 * 60);
-  }
-
-  useEffect(() => {
-    if (isActive && time > 0) {
-      countDownTimeout = setTimeout(() => {
-        setTime(time - 1);
-      }, 1000);
-    } else if (isActive && time === 0) {
-      sethasFinished(true);
-      setisActive(false);
-      startNewChallenge();
-    }
-  }, [isActive, time]);
 
   return (
     <div>
