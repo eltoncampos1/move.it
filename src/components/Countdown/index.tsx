78e9a15161cacb6d05ef1f7cf6959eeae7d7d897
elton de campos <eltoncampos36@gmail.com>
2021-03-01 02:11:10 -0300

    chore: import necessaries dependences to countdown components

--- src/components/Countdown/index.tsx
+++ src/components/Countdown/index.tsx
@@ -1,3 +1,5 @@
+import { useContext } from "react";
+import { CountdownContext } from "../../contextx/CountdownContext";
 import {
   Container,
   CountdownButton,
@@ -6,6 +8,15 @@ import {
 } from "./styles";
 
 export function Countdown() {
+  const {
+    seconds,
+    minutes,
+    hasFinished,
+    isActive,
+    startCountdown,
+    resetCountdown,
+  } = useContext(CountdownContext);
+
   const [minuteLeft, minuteRight] = String(minutes).padStart(2, "0").split("");
   const [secondLeft, secondRight] = String(seconds).padStart(2, "0").split("");
 
